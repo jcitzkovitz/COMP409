cc6c444b25a25a80f0498a232cf1db114fc5768f
Jordan C. Itzkovitz <jcitzko@hotmail.com>
2019-02-19 18:18:11 -0500

    Commented q2

--- A2/q2.java
+++ A2/q2.java
@@ -28,6 +28,14 @@ class Point {
         return "("+x+","+y+")";
     }
 
+    /**
+     * Get angle between two vectors, including this point
+     *
+     * @param Point p is a point on one vector
+     * @param Point q is a point on the other vector
+     *
+     * @return the angle between the two vectors
+     * */
     public double getAngle(Point p, Point q){
         double uLen = Math.sqrt(Math.pow(p.x-this.x,2) + Math.pow(p.y-this.y, 2));
         double vLen = Math.sqrt(Math.pow(q.x-this.x,2) + Math.pow(q.y-this.y, 2));
@@ -36,6 +44,9 @@ class Point {
       }
 }
 
+/**
+ * Class for a pair of Points
+ * */
 class PointPair {
 	Point p1;
 	Point p2;
@@ -81,6 +92,11 @@ class Edge extends Semaphore{
     	return this.p.same(f.p) && this.q.same(f.q);
     }
 
+    /**
+     * Function that chooses the point on an edge that is not the one specified
+     *
+     * @param Point p is the point
+     * */
     public Point getOppositePoint(Point p){
     	return this.p.same(p) ? this.q : this.p;
     }
@@ -93,7 +109,12 @@ public class q2 {
     public static volatile int flipCount = 0;
     public static Point[] points;
     public static volatile ArrayList<Edge> edges = new ArrayList<Edge>();
-    public static Semaphore removeLock = new Semaphore(1);
+
+    // Use a lock for any thread that is attempting to flip an edge
+    public static Semaphore flipLock = new Semaphore(1);
+
+    // Maintain a list of removed edges so that threads that are looking at a removed edge
+    // realize that it has in fact been removed.
     public static ArrayList<Edge> removedEdges = new ArrayList<Edge>();
 
     // Returns true if any existing edge intersects this one
@@ -106,24 +127,33 @@ public class q2 {
         return false;
     }
 
-    // Returns true if the number of edges this edge crosses is less equal to 1
-    public static boolean isCandidatePair(Edge f) {
-    	int count = 0;
-        for (int i = 0; i < edges.size(); i++) {
-            if (f.intersects(edges.get(i))) {
-                count++;
-                if(count > 1)
-                	return false;
+    /**
+     *  Returns true if the number of edges this edge crosses is exactly equal to 1 and is the edge that is to be removed
+     *
+     *  @param Edge f is the edge that we would like to know if it crosses exactly 1 edge and the edge to be removed
+     *  @param Edge rem is the edge that is to be removed
+     * */
+    public static boolean isCandidatePair(Edge f, Edge rem) {
+    	if(f.intersects(rem)){
+    		int count = 0;
+            for (int i = 0; i < edges.size(); i++) {
+                if (f.intersects(edges.get(i))) {
+                    count++;
+                    if(count > 1)
+                    	return false;
+                }
             }
-        }
 
-        if(count != 1)
-        	return false;
-        else
-        	return true;
+            if(count == 1)
+            	return true;
+    	}
+    	return false;
     }
 
-    public static synchronized void updateCount(){
+    /**
+     * Synchronized function for updating the flipCount
+     * */
+    public static synchronized void updateFlipCount(){
     	flipCount++;
     }
 
@@ -178,14 +208,12 @@ public class q2 {
                 }
             }
             System.out.println("Triangulated: "+n+" points, "+edges.size()+" edges");
-            System.out.println(edges.toString());
 
-            // Now your code is required!
             ExecutorService executor = Executors.newFixedThreadPool(t);
 
-            // Execute the piece threads.
+            // Execute the Delaunay threads.
  			for(int i = 0; i < t; i++){
- 				executor.execute(new Delaunay(i));
+ 				executor.execute(new Delaunay());
  			}
 
  			executor.shutdown();
@@ -199,17 +227,22 @@ public class q2 {
         }
     }
 
+    /**
+     * Thread class that runs Delaunay triangulation on the given set of edges
+     * */
     static class Delaunay implements Runnable{
 
-    	int id;
-
-    	public Delaunay(int id){
-    		this.id = id;
-    	}
-
+    	 /**
+    	  * Get all pair of points that may be used for flipping associated with Edge e
+    	  *
+    	  * @param Edge e is the edge that is being inspected for flip candidates.
+    	  * */
     	 private ArrayList<PointPair> getFlipCandidates(Edge e){
+
     	    	ArrayList<PointPair> candidates = new ArrayList<PointPair>();
     	    	ArrayList<Point> connectingPoints = new ArrayList<Point>();
+
+    	    	// Retrieve all points that connect point p and q by an edge other than their respective edge.
     	    	for(int i = 0; i < e.p.edges.size(); i++){
     	    		Edge pe = e.p.edges.get(i);
     	    			for(int j = 0; j < e.q.edges.size(); j++){
@@ -223,23 +256,32 @@ public class q2 {
     	    		}
     	    	}
 
+    	    	// Permute the points retrieved from the previous step and test id they are real candidates for flipping.
+    	    	// A candidate pair creates an edge that only crosses the edge that is to be removed.
     	    	for(int i = 0; i < connectingPoints.size(); i++){
     	    		for(int j = i+1; j < connectingPoints.size(); j++){
     	    			PointPair pair = new PointPair(connectingPoints.get(i),connectingPoints.get(j));
     	    			Edge f = new Edge(pair.p1,pair.p2);
-    	    			if(isCandidatePair(f) && f.intersects(e))
+    	    			if(isCandidatePair(f,e))
     	    				candidates.add(pair);
     	    		}
     	    	}
     	    	return candidates;
     	}
 
-    	private boolean isFlipable(Edge e, PointPair pair){
+    	/**
+    	 * Checks whether a pair of points is attached to a specific edge by an edge that has been removed from the graph
+    	 *
+    	 * @param Edge e is the edge to be examined.
+    	 * @param PointPair p is the pair of points to be examined.
+    	 * */
+    	private boolean isStillFlippable(Edge e, PointPair pair){
     		Edge test1 = new Edge(e.p,pair.p1);
     		Edge test2 = new Edge(e.q,pair.p1);
     		Edge test3 = new Edge(e.p,pair.p2);
     		Edge test4 = new Edge(e.q, pair.p2);
 
+    		// Check if the edges are in the removedEdges list, and if so return false.
     		for(int i = 0; i < removedEdges.size(); i++){
     			Edge r = removedEdges.get(i);
     			if(r.equals(test1) || r.equals(test2) || r.equals(test3) || r.equals(test4))
@@ -248,36 +290,65 @@ public class q2 {
     		return true;
     	}
 
+    	/**
+    	 * Function that does all necessary actions for flipping an edge
+    	 *
+    	 * @param Edge remEdge is the edge to be removed
+    	 * @param PointPair is the pair of points that are to form a new Edge in the graph.
+    	 * */
         private void flipEdge(Edge remEdge, PointPair addPair){
+
+        	// Remove the edge to be flipped from the graph and add it to the list of removed edges.
+        	edges.remove(remEdge);
         	removedEdges.add(remEdge);
-			edges.remove(remEdge);
+
+        	// Create a new edge between the two given points and add it to the graph
     		Edge newEdge = new Edge(addPair.p1,addPair.p2);
     		edges.add(newEdge);
+
+    		// Remove the flipped Edge from the two points and add their new edges.
     		addPair.p1.edges.remove(remEdge);
     		addPair.p2.edges.remove(remEdge);
     		addPair.p1.edges.add(newEdge);
     		addPair.p2.edges.add(newEdge);
     		System.out.println("Flipped: "+newEdge.toString());
-    		updateCount();
+
+    		// Update the flip count
+    		updateFlipCount();
         }
 
 
     	public void run(){
+
+    		// While there remain flips to be made, continue traversing all edges.
     		boolean flips = false;
             do{
             	flips = false;
             	for(int i = 0; i < edges.size(); i++){
+
             		Edge e = edges.get(i);
+
+            		// Attempt to acquire the lock on e. If not, move onto the next edge.
             		if(e.tryAcquire()){
-            			System.out.println("Thread "+this.id+" got edge "+e.toString());
+
+            			// Get the potential candidate points for flipping Edge e and check if they should be flipped.
             			ArrayList<PointPair> candidates = getFlipCandidates(e);
         				for(int j = 0; j < candidates.size(); j++){
             				PointPair pair = candidates.get(j);
+
+            				// Calculate the angles created from the two point connecting to e.p and e.q.
                         	double angle1 = pair.p1.getAngle(e.p,e.q);
                         	double angle2 = pair.p2.getAngle(e.p,e.q);
+
+                        	// If the angle is greater than PI radians, attempt to flip the edge.
                         	if(angle1+angle2 > Math.PI){
-                        		synchronized(removeLock){
-                        			if(isFlipable(e,pair)){
+
+                        		// Obtain the flipLock
+                        		synchronized(flipLock){
+
+                        			// If the edge is still flippable (ie a connecting edge to e still exists) then flip it.
+                        			// Otherwise, move onto the next edge.
+                        			if(isStillFlippable(e,pair)){
                         				flipEdge(e,pair);
                         				flips = true;
                         			}else{
@@ -286,6 +357,8 @@ public class q2 {
                         		}
                         	}
                         }
+
+        				// Release the lock on e
         				e.release();
             		}else
             			continue;
