0cd415cf98efad4be71d15aa75ca4964d2bac0bf
Jordan C. Itzkovitz <jcitzko@hotmail.com>
2019-02-18 19:21:40 -0500

    Commented code - still need to remove testing print statements

--- A2/q1.java
+++ A2/q1.java
@@ -6,13 +6,17 @@ import java.util.concurrent.Semaphore;
 
 public class q1 {
 
+	// Board size of 8x8.
 	static final int BOARD_SIZE = 8;
+
+	// Global variables regarding chess pieces, the board and the move count.
 	static int numPieces;
 	static ChessPiece[] pieces;
 	static volatile Semaphore[][] board;
 	static volatile boolean run;
 	static volatile int moveCount;
 
+	// Global variables regarding synchronization for instantaneously accurate printing.
 	static volatile boolean printReady;
 	static volatile int numPrintReady;
 	static Semaphore printLock;
@@ -26,18 +30,20 @@ public class q1 {
 			numPieces = 0;
 			double runTime = 0;
 			try{
-				numPieces = Integer.parseInt(args[0]);
+				numPieces = Math.min(64, Integer.parseInt(args[0]));
 				runTime = Double.parseDouble(args[1]);
 			} catch (NumberFormatException e){
 				System.out.println(invalidArgs);
 				System.exit(0);
 			}
 
+			// Set the board, pieces and locks
 			board = new Semaphore[BOARD_SIZE][BOARD_SIZE];
 			pieces = new ChessPiece[numPieces];
 			printLock = new Semaphore(1);
 			printReadyLock = new Semaphore(1);
 
+			// Initialize pieces on the board (row by row).
 			int xPos = 0;
 			int yPos = -1;
 			int i = 0;
@@ -45,12 +51,15 @@ public class q1 {
 				xPos = i%BOARD_SIZE;
 				if(xPos == 0)
 					yPos++;
-				ChessPiece piece = Math.random() <= 0.5 ? new Queen(xPos,yPos,i) : new Knight(xPos,yPos,i);
+
+				// Randomly select whether to place a Queen or a Knight at the (xPos,yPos)th position on the board.
+				ChessPiece piece = Math.random() <= 0.5 ? new Queen(xPos,yPos) : new Knight(xPos,yPos);
 				pieces[i] = piece;
 				board[yPos][xPos] = piece;
 				i++;
 			}
 
+			// Initialize all board positions that do not have a chess piece to be a lock.
 			for(i = 0; i < BOARD_SIZE; i++){
 				for(int j = 0; j < BOARD_SIZE; j++){
 					if(board[i][j] == null){
@@ -62,12 +71,19 @@ public class q1 {
 			printBoard();
 
 			run = true;
+
+			// Set the number of threads to run equal to the number of pieces, plus another thread for the printing mechanism.
 			ExecutorService executor = Executors.newFixedThreadPool(numPieces+1);
+
+			// Execute the print mechanism thread.
+			executor.execute(new PrintCount());
+
+			// Execute the piece threads.
 			for(i = 0; i < numPieces; i++){
 				executor.execute(pieces[i]);
 			}
 
-			executor.execute(new PrintCount());
+			// Run the program for the specified amount of time given at run time.
 			long start = System.currentTimeMillis();
 			while(System.currentTimeMillis()-start < 1000*runTime);
 			run = false;
@@ -75,19 +91,29 @@ public class q1 {
 			executor.shutdown();
 			while (!executor.isTerminated());
 
+			// Print the final move count.
 			System.out.println("Final moves made: "+getMoveCount());
 			printBoard();
 		}
 	}
 
+	/**
+	 * Synchronized method for updating the moveCount.
+	 * */
 	synchronized static void updateCount(){
 		moveCount++;
 	}
 
+	/**
+	 * Synchronized method for getting the moveCount.
+	 * */
 	synchronized static int getMoveCount(){
 		return moveCount;
 	}
 
+	/**
+	 * Print the board layout - for testing purposes
+	 * */
 	static void printBoard(){
 		for(int i = 0; i < BOARD_SIZE; i++){
 			for(int j = 0; j < BOARD_SIZE; j++){
@@ -103,10 +129,14 @@ public class q1 {
 		}
 	}
 
+	/**
+	 * Thread for printing the moveCount with instantaneous accuracy.
+	 * */
 	static class PrintCount implements Runnable{
 		public void run(){
 			while(run){
 
+				// Set the the threads intention to print, and wait for all threads to tell this thread that they've paused execution.
 				synchronized(printReadyLock){
 					try {
 //						System.out.println("Ready to print...");
@@ -117,16 +147,15 @@ public class q1 {
 						e.printStackTrace();
 					}
 				}
-				synchronized(printLock){}
 
-				System.out.println("Moves made at current stage: "+getMoveCount());
-//				printBoard();
-
-				printReady = false;
+				// Obtain the printLock, print, and then notify all threads that they may continue execution.
 				synchronized(printLock){
+					System.out.println("Moves made at current stage: "+getMoveCount());
+					printReady = false;
 					printLock.notifyAll();
 				}
 
+				// Sleep this thread for 1 second.
 				try {
 					Thread.sleep(1000);
 				} catch (InterruptedException e) {
@@ -137,17 +166,29 @@ public class q1 {
 		}
 	}
 
+	/**
+	 * ChessPiece abstract class
+	 *
+	 * Each chess piece itself is a lock to be placed on the board of empty locks.
+	 * */
 	static abstract class ChessPiece extends Semaphore implements Runnable{
+		// X and y position of the piece on the board
 		protected int x;
 		protected int y;
-		protected int id;
 
-		public ChessPiece(int x, int y, int id){
+		/**
+		 * Constructor
+		 *
+		 * @param int x is the x position of the piece on the board.
+		 * @param int y is the y position of the piece on the board.
+		 * */
+		public ChessPiece(int x, int y){
+			// Set itself to be a Semaphore with 1 resource.
 			super(1);
 			this.x = x;
 			this.y = y;
-			this.id = id;
 			try {
+				// Acquire its own lock
 				super.acquire();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
@@ -156,17 +197,29 @@ public class q1 {
 
 		public abstract void run();
 		volatile int syncCount = 0;
+
+		/**
+		 * Function for synchronizing ChessPiece execution with the PrintCount thread
+		 *
+		 * When the PrintCount thread sets its intention to print, wait for all ChessPiece threads to stop execution
+		 * notify the PrintCount thread that it may continue execution, and wait to be notified that it is done.
+		 * */
 		protected void printCount(){
+			// If PrintCount sets its intention to print, increase the numPrintReady and wait to be notified that
+			// the printing mechanism is done execution.
 			if(printReady){
 //				System.out.println(++this.syncCount+" waiting at sync...");
 				synchronized(printLock){
 					try {
 						numPrintReady++;
+
+						// If all threads are now waiting on the PrintCount thread, notify PrintCount to perform its
+						// execution.
 						if(numPrintReady == numPieces){
 							synchronized(printReadyLock){
 								numPrintReady = 0;
 //								System.out.println("Notify printer!");
-								printReadyLock.notifyAll();
+								printReadyLock.notify();
 							}
 						}
 //						System.out.println(numPrintReady+" waiting for printLock - total: ");
@@ -179,18 +232,29 @@ public class q1 {
 		}
 	}
 
+	/**
+	 * Queen ChessPiece
+	 * */
 	static class Queen extends ChessPiece {
 
-		public Queen(int x, int y,int id){
-			super(x,y,id);
+		/**
+		 * Constructor
+		 *
+		 * @param int x is the x position of this piece on the board.
+		 * @param int y is the y position of this piece on the board.
+		 * */
+		public Queen(int x, int y){
+			super(x,y);
 		}
 
 		@Override
 		public void run() {
 			while(run){
 
+				// Check the PrintCount threads intention to print, and wait for permission to continue execution.
 				printCount();
 
+				// Generate a random number to decide what move this Piece will make.
 				int randomDir = (int) Math.floor((4*Math.random()));
 				int steps = 0;
 
@@ -210,11 +274,12 @@ public class q1 {
 				else if(randomDir == 3)
 					steps = moveCol(true);
 
-
+				// If the random move generated cannot be performed, restart the loop
 				if(steps == 0){
 					continue;
 				}
 				else{
+					// Update the count and sleep for 10-30 ms.
 					updateCount();
 					try {
 						Thread.sleep((long) (10+20*Math.random()));
@@ -226,13 +291,27 @@ public class q1 {
 
 		}
 
+		/**
+		 * Function that generates the Queen's next vertical move
+		 *
+		 * @param boolean posDirection decides which direction the piece will move vertically (+ = down, - = up).
+		 *
+		 * @return the number of steps covered.
+		 * */
 		private int moveRow(boolean posDirection){
+			// Generate a random step.
 			int steps = randomStep(this.y,posDirection);
+
+			// Take note of the number of stepsCovered, the direction and the starting position.
 			int stepsCovered = 0;
 			int sign = posDirection ? 1 : -1;
 			int startPos = this.y;
+
+			// Store the previous acquired lock to keep note of how far this piece may travel.
 			Semaphore prev = board[this.y][this.x];
 
+			// While the number of steps covered is less than the intended amount and this piece
+			// can acquire the next lock (ie. safely move to the next position), continue stepping.
 			while(stepsCovered < steps){
 				Semaphore l = board[startPos+sign*(stepsCovered+1)][this.x];
 				if(l.tryAcquire()){
@@ -243,14 +322,17 @@ public class q1 {
 				}
 			}
 
+			// Swap the piece with the last locked position and update its y position.
 			board[startPos+sign*(stepsCovered)][this.x] = this;
 			board[this.y][this.x] = prev;
 			this.y = startPos+sign*stepsCovered;
+
+			// Release all locks acquired on the path to its current position.
 			for(int i = 0; i < stepsCovered; i++){
 				try{
-					board[startPos+sign*i][this.x].release();;
+					board[startPos+sign*i][this.x].release();
 				}catch(Exception e){
-					System.out.println("ROW: is queen in T"+this.id+"? "+(board[startPos+sign*i][this.x] instanceof Queen));
+					System.out.println("ROW: is queen? "+(board[startPos+sign*i][this.x] instanceof Queen));
 					System.exit(0);
 				}
 			}
@@ -258,13 +340,27 @@ public class q1 {
 			return stepsCovered;
 		}
 
+		/**
+		 * Function that generates the Queen's next horizontal move
+		 *
+		 * @param boolean posDirection decides which direction the piece will move horizontally (+ = right, - = left).
+		 *
+		 * @return the number of steps covered.
+		 * */
 		private int moveCol(boolean posDirection){
+			// Generate a random step
 			int steps = randomStep(this.x,posDirection);
+
+			// Take note of the number of stepsCovered, the direction and the starting position.
 			int stepsCovered = 0;
 			int sign = posDirection ? 1 : -1;
 			int startPos = this.x;
+
+			// Store the previous acquired lock to keep note of how far this piece may travel.
 			Semaphore prev = board[this.y][this.x];
 
+			// While the number of steps covered is less than the intended amount and this piece
+			// can acquire the next lock (ie. safely move to the next position), continue stepping.
 			while(stepsCovered < steps){
 				Semaphore l = board[this.y][startPos+sign*(stepsCovered+1)];
 				if(l.tryAcquire()){
@@ -275,14 +371,17 @@ public class q1 {
 				}
 			}
 
+			// Swap the piece with the last locked position and update its x position.
 			board[this.y][startPos+sign*(stepsCovered)] = this;
 			board[this.y][this.x] = prev;
 			this.x = startPos+sign*stepsCovered;
+
+			// Release all locks acquired on the path to its current position.
 			for(int i = 0; i < stepsCovered; i++){
 				try{
 					board[this.y][startPos+sign*i].release();
 				}catch(Exception e){
-					System.out.println("COL: is queen in T"+this.id+"? "+(board[this.y][startPos+sign*i] instanceof Queen));
+					System.out.println("COL: is queen? "+(board[this.y][startPos+sign*i] instanceof Queen));
 					System.exit(0);
 				}
 			}
@@ -290,9 +389,17 @@ public class q1 {
 			return stepsCovered;
 		}
 
+		/**
+		 * Generate a random number of steps to take
+		 *
+		 * @param int start is the starting position of the piece.
+		 * @param boolean posDirection is the direction the piece will move in.
+		 * */
 		private int randomStep(int start, boolean posDirection){
 			double rand = Math.random();
 			rand = rand == 0.0 ? 1.0 : rand;
+
+			// Generate a number of steps that is within the confines of the board.
 			if(posDirection)
 				return (int) Math.ceil((BOARD_SIZE-start-1)*rand);
 			else
@@ -300,22 +407,32 @@ public class q1 {
 		}
 	}
 
+	/**
+	 * Knight ChessPiece
+	 * */
 	static class Knight extends ChessPiece {
 
-		private int x;
-		private int y;
-
-		public Knight(int x, int y, int id){
-			super(x,y,id);
+		/**
+		 * Constructor
+		 *
+		 * @param int x is the x position of this piece on the board.
+		 * @param int y is the y position of this piece on the board.
+		 * */
+		public Knight(int x, int y){
+			super(x,y);
 		}
 
 		@Override
 		public void run() {
 			while(run){
 
+				// Check the PrintCount threads intention to print, and wait for permission to continue execution.
 				printCount();
 
+				// Generate a random direction for this piece to move in
 				int randomDir = (int) Math.floor((4*Math.random()));
+
+				// Generate a random side of the jump to be on
 				boolean side = Math.random() <= 0.5 ? true : false;
 				boolean didStep = false;
 
@@ -335,11 +452,12 @@ public class q1 {
 				else if(randomDir == 3)
 					didStep = moveCol(true, side);
 
-
+				// If the step could not be covered, generate a new move.
 				if(!didStep){
 					continue;
 				}
 				else{
+					// Update the count and sleep for 10-30 ms.
 					updateCount();
 					try {
 						Thread.sleep((long) (10+20*Math.random()));
@@ -350,44 +468,59 @@ public class q1 {
 			}
 		}
 
+		/**
+		 * Function that generates the Knight's next vertical move
+		 *
+		 * @param boolean posDirection decides which direction the piece will move vertically (+ = down, - = up).
+		 * @param boolean leftSide decides which side of the vertical move to land on (true = left, false = right).
+		 *
+		 * @return the number of steps covered.
+		 * */
 		private boolean moveRow(boolean posDirection, boolean leftSide){
+
+			// Decide which direction to move in, and which side of the jump to land on.
 			int moveDirection = posDirection ? 3 : -3;
 			int side = leftSide ? -1 : 1;
 
-			if(this.y+moveDirection >= 0 && this.y+moveDirection <= BOARD_SIZE-1){
-				if(this.x+side >= 0 && this.x+side <= BOARD_SIZE-1){
-					Semaphore l = board[this.y+moveDirection][this.x+side];
-					if(l.tryAcquire()){
-						board[this.y+moveDirection][this.x+side] = this;
-						board[this.y][this.x] = l;
-						board[this.y][this.x].release();
-						this.y+=moveDirection;
-						this.x+=side;
-						return true;
-					}else
-						return false;
+			// If the move is within the bounds of the board, attempt to lock the new position and move there
+			if(this.y+moveDirection >= 0 && this.y+moveDirection <= BOARD_SIZE-1 && this.x+side >= 0 && this.x+side <= BOARD_SIZE-1){
+				Semaphore l = board[this.y+moveDirection][this.x+side];
+				if(l.tryAcquire()){
+					board[this.y+moveDirection][this.x+side] = this;
+					board[this.y][this.x] = l;
+					board[this.y][this.x].release();
+					this.y+=moveDirection;
+					this.x+=side;
+					return true;
 				}else
 					return false;
 			}else
 				return false;
 		}
 
+		/**
+		 * Function that generates the Knight's next horizontal move
+		 *
+		 * @param boolean posDirection decides which direction the piece will move horizontally (+ = right, - = left).
+		 * @param boolean downSide decides which side of the horizontal move to land on (true = below, false = above).
+		 *
+		 * @return the number of steps covered.
+		 * */
 		private boolean moveCol(boolean posDirection, boolean downSide){
+			// Decide which direction to move in, and on which side of the jump to land on.
 			int moveDirection = posDirection ? 3 : -3;
 			int side = downSide ? -1 : 1;
 
-			if(this.x+moveDirection >= 0 && this.x+moveDirection <= BOARD_SIZE-1){
-				if(this.y+side >= 0 && this.y+side <= BOARD_SIZE-1){
-					Semaphore l = board[this.y+side][this.x+moveDirection];
-					if(l.tryAcquire()){
-						board[this.y+side][this.x+moveDirection] = this;
-						board[this.y][this.x] = l;
-						board[this.y][this.x].release();
-						this.y+=side;
-						this.x+=moveDirection;
-						return true;
-					}else
-						return false;
+			// If the move is within the bounds of the board, attempt to the lock the new position and move there.
+			if(this.x+moveDirection >= 0 && this.x+moveDirection <= BOARD_SIZE-1 && this.y+side >= 0 && this.y+side <= BOARD_SIZE-1){
+				Semaphore l = board[this.y+side][this.x+moveDirection];
+				if(l.tryAcquire()){
+					board[this.y+side][this.x+moveDirection] = this;
+					board[this.y][this.x] = l;
+					board[this.y][this.x].release();
+					this.y+=side;
+					this.x+=moveDirection;
+					return true;
 				}else
 					return false;
 			}else
